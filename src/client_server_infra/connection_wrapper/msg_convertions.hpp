76de28ed9bf03c52565150629cc921a6a4d2039f
bulat <sinntexxx@gmail.com>
2024-02-16 17:02:00 +0300

    Deleted method getMessage(). Now connection wrapper and request are autonomous

--- src/client_server_infra/connection_wrapper/msg_convertions.hpp
+++ src/client_server_infra/connection_wrapper/msg_convertions.hpp
@@ -3,15 +3,9 @@
 
 #include "zmq.hpp"
 
-#include <request/commands/commands_module.hpp>
-
 namespace lab5_7 {
     zmq::message_t to_msg(std::string const& str) {
         return zmq::message_t(str);
-    }
-
-    zmq::message_t to_msg(Command::cmd_ptr cmd) {
-        return cmd->getMessage();
     }
 
     std::string to_string(zmq::message_t const& msg) {
